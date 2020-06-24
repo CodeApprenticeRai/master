5afc313eccccf2ea6342b4129ffce5606e88f8e6
chaselee <chaselee.13@gmail.com>
2020-06-24 09:47:43 -0600

    scorecard added

--- master/app/models.py
+++ master/app/models.py
@@ -2,52 +2,74 @@ from django.db import models
 
 
 class User(models.Model):
-    name=models.CharField(max_length=200)
-    #username
-    #email
-    #etc
+    name = models.CharField(max_length=200)
+
+    # username
+    # email
+    # etc
 
     def __str__(self):
         return self.name
+
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
-       return self.name
+        return self.name
+
 
 class InstructorRole(models.Model):
-    associated_user=models.ForeignKey(User, on_delete=models.PROTECT)
-    associated_course= models.ForeignKey(Course, on_delete=models.PROTECT)
+    associated_user = models.ForeignKey(User, on_delete=models.PROTECT)
+    associated_course = models.ForeignKey(Course, on_delete=models.PROTECT)
 
     def __str__(self):
-       return str(self.associated_user.name)
+        return str(self.associated_user.name)
+
 
 class Skill(models.Model):
     name = models.CharField(max_length=255)
     parent_course = models.ForeignKey(Course, on_delete=models.CASCADE)
 
     def __str__(self):
-       return str(self.name)
+        return str(self.name)
+
 
 class Challenge(models.Model):
     name = models.CharField(max_length=255)
     parent_skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
 
     def __str__(self):
-       return self.name
+        return self.name
+
 
 class Question(models.Model):
     parent_challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
     text = models.TextField()
 
     def __str__(self):
-       return self.text
+        return self.text
+
 
 class QuestionChoice(models.Model):
-    parent_question = models.ForeignKey(Question, on_delete=models.CASCADE )
+    parent_question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.TextField()
     correct_answer = models.BooleanField()
 
     def __str__(self):
-       return self.text
+        return self.text
+
+
+class Scorecard(models.Model):
+    associated_challenge = models.ForeignKey(Challenge, on_delete=models.PROTECT)
+    associated_user = models.ForeignKey(User, on_delete=models.PROTECT)
+    score = models.IntegerField()
+
+    def __init__(self, chall, use, points):
+        self.associated_challenge = chall
+        self.associated_user = use
+        self.score = points
+
+    def __str__(self):
+        return self.score
+
--- master/app/views.py
+++ master/app/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from .models import *
 import app.forms
 
+
 def index(request):
     return render(request, 'app/index.html')
+
 
 def challenge(request, challenge_id):
     if request.method == 'POST':
@@ -16,22 +18,27 @@ def challenge(request, challenge_id):
 
         # print(request.POST)
         for form_question in request.POST:
-            if ( form_question == "csrfmiddlewaretoken" ):
+            if form_question == "csrfmiddlewaretoken":
                 continue
 
             user_choice_id = request.POST[form_question]
             choice_obj = QuestionChoice.objects.get(id=user_choice_id)
 
             print([choice_obj.id, choice_obj.text, choice_obj.correct_answer])
-            if (choice_obj.correct_answer):
+            if choice_obj.correct_answer:
                 correct_answer_count += 1
 
             form_question_count += 1
 
+        # generate scorecard
+        chal = Challenge.objects.get(id=challenge_id)
+        perc = ((correct_answer_count / form_question_count) * 100)
+        grade = Scorecard(chal, request.user.id, perc)
+
         context = {
             'correct_answer_count': correct_answer_count,
             'form_question_count': form_question_count,
-            'percentage_correct': (correct_answer_count /  form_question_count) * 100
+            'percentage_correct': (correct_answer_count / form_question_count) * 100
         }
 
         return render(request, 'app/challenge_report.html', context)
@@ -45,14 +52,17 @@ def challenge(request, challenge_id):
         }
         return render(request, 'app/challenge.html', context)
 
+
 # !! For now ( until moving to sessions ) : Given an instructor_id,
 # return tablelists of all courses that user is an instructor of
+
 def instructor_view_courses(request, instructor_id):
     # get instructor object
     instructor_obj = User.objects.get(id=instructor_id)
 
     # extract associated_courses
-    courses_led_by_instructor = [ role_object.associated_course for role_object in InstructorRole.objects.filter(associated_user=instructor_obj) ]
+    courses_led_by_instructor = [role_object.associated_course for role_object in
+                                 InstructorRole.objects.filter(associated_user=instructor_obj)]
 
     # add courses to context
     context = {
@@ -62,22 +72,67 @@ def instructor_view_courses(request, instructor_id):
 
     return render(request, 'app/instructor_view_courses.html', context)
 
+
 def instructor_view_course_skills(request, course_id):
     course_obj = Course.objects.get(id=course_id)
     skills = Skill.objects.filter(parent_course=course_obj)
 
-    context={
+    context = {
         "course": course_obj,
         "skills": skills
     }
 
     return render(request, 'app/instructor_view_course_skills.html', context)
 
+
 def instructor_view_skill_challenges(request, skill_id):
     skill_obj = Skill.objects.get(id=skill_id)
     challenges = Challenge.objects.filter(parent_skill=skill_obj)
+    # grade = Scorecard.objects.filter()
 
-    context={
+    context = {
+        "skill": skill_obj,
+        "challenges": challenges
+    }
+
+    return render(request, 'app/instructor_view_skill_challenges.html', context)
+
+
+def instructor_view_courses(request, instructor_id):
+    # get instructor object
+    instructor_obj = User.objects.get(id=instructor_id)
+
+    # extract associated_courses
+    courses_led_by_instructor = [role_object.associated_course for role_object in
+                                 InstructorRole.objects.filter(associated_user=instructor_obj)]
+
+    # add courses to context
+    context = {
+        "instructor": instructor_obj,
+        "courses": courses_led_by_instructor
+    }
+
+    return render(request, 'app/instructor_view_courses.html', context)
+
+
+def instructor_view_course_skills(request, course_id):
+    course_obj = Course.objects.get(id=course_id)
+    skills = Skill.objects.filter(parent_course=course_obj)
+
+    context = {
+        "course": course_obj,
+        "skills": skills
+    }
+
+    return render(request, 'app/instructor_view_course_skills.html', context)
+
+
+def instructor_view_skill_challenges(request, skill_id):
+    skill_obj = Skill.objects.get(id=skill_id)
+    challenges = Challenge.objects.filter(parent_skill=skill_obj)
+    # grade = Scorecard.objects.filter()
+
+    context = {
         "skill": skill_obj,
         "challenges": challenges
     }
